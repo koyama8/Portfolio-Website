0e11124000bf0192941942ad62f883b8423fe52c
Matheus Koyama <koyamaCRS@gmail.com>
2024-06-10 18:33:03 -0300

    - Finalizando o navbar - Ajustes gerais na responsividade

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+/*  navbar */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
+
 let sections = document.querySelectorAll('secton');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -16,6 +25,12 @@ window.onscroll = () => {
         };
     });
 
+    let header = document.querySelector('header')
+
     /* navbar */
     header.classList.toggle('sticky',window.scrollY > 100);
+
+    /*   */
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active')
 };
